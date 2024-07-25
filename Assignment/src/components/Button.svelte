fdc948d1afe320d5c4e4adcc7703cd0150ea2c01
jnn_sn <xmissj96@gmail.com>
2024-07-25 14:55:50 +0200

    feat: added button component and navigation to assignment pages.

---
+++ Assignment/src/components/Button.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let width: number | null = null;
+  export let onClick: Function;
+
+  function handleClick() {
+    onClick();
+  }
+</script>
+
+<button
+  class="button"
+  style="width: {width == null ? 'fit-content' : width + 'px'};"
+  on:click={handleClick}
+>
+  <slot />
+</button>
+
+<style lang="scss">
+  .button {
+    display: inline-block;
+    height: fit-content;
+    padding: 10px;
+    cursor: pointer;
+    border: none;
+    border-radius: 14px;
+    background-color: white;
+
+    transition:
+      background-color 0.2s,
+      color 0.2s;
+
+    &:hover {
+      background-color: var(--color-button-bg);
+      color: white;
+    }
+  }
+</style>
--- Assignment/src/routes/+page.svelte
+++ Assignment/src/routes/+page.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { base } from "$app/paths";
+  import Button from "../components/Button.svelte";
+
+  let pages = [
+    { text: "Form Picker", path: "form-picker" },
+    { text: "Web Design", path: "web-design" },
+    { text: "Case", path: "case" },
+  ];
 </script>
 
 <svelte:head>
@@ -6,7 +15,28 @@
   <meta name="description" content="Home screen with selectable assignments." />
 </svelte:head>
 
-<section></section>
+<section id="home">
+  <div id="assignment-selection">
+    {#each pages as page}
+      <Button width={120} onClick={() => goto(base + page.path)}>
+        {page.text}
+      </Button>
+    {/each}
+  </div>
+</section>
 
 <style>
+  #home {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  #assignment-selection {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+  }
 </style>
