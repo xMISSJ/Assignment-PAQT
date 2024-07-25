a9a4ac90795cbecbf87bd0282920f078c89913ee
jnn_sn <xmissj96@gmail.com>
2024-07-25 13:48:04 +0200

    feat: initial commit.

---
+++ Assignment/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Home screen with selectable assignments." />
+</svelte:head>
+
+<section></section>
+
+<style>
+</style>
